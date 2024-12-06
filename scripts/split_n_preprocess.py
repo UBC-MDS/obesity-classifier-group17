7c6c3a5f2596fff57bb755deb3c44ca66b5cf6b8
Green-zy <zhou165@usc.edu>
2024-12-06 09:48:54 -0800

    updated fit model scripts and eveluate scripts with results, updated notebook

--- scripts/split_n_preprocess.py
+++ scripts/split_n_preprocess.py
@@ -4,19 +4,20 @@
 
 import click
 import os
+import pickle
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.compose import make_column_transformer, make_column_selector
-from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from sklearn.compose import ColumnTransformer
 
 @click.command()
 @click.option('--clean-data', type=str, help="Path to the data after cleaning")
 @click.option('--data-to', type=str, help="Path to directory where splitted data will be written to")
-#@click.option('--preprocessor-to', type=str, help="Path to directory where the preprocessor object will be written to")
+@click.option('--preprocessor-to', type=str, help="Path to directory where the preprocessor object will be written to")
 @click.option('--seed', type=int, help="seed for random state", default=522)
-def main(clean_data, data_to, seed):
+def main(clean_data, data_to, preprocessor_to, seed):
     '''This script splits the data after cleaning for training and testing.
     EDA will be performed on training data set.
     The preprocessor will be applied for model training.'''
@@ -43,6 +44,37 @@ def main(clean_data, data_to, seed):
     # Store splitted data
     train_df.to_csv(os.path.join(data_to, "obesity_train.csv"), index=False)
     test_df.to_csv(os.path.join(data_to, "obesity_test.csv"), index=False)
+
+    # Encode target variable
+    label_encoder = LabelEncoder()
+    label_encoder.fit(y_train)
+
+    train_df['obesity_level'] = label_encoder.transform(train_df['obesity_level'])
+    test_df['obesity_level'] = label_encoder.transform(test_df['obesity_level'])
+
+    train_df.to_csv(os.path.join(data_to, "obesity_train_target_encoding.csv"), index=False)
+    test_df.to_csv(os.path.join(data_to, "obesity_test_target_encoding.csv"), index=False)
+
+    # Identify categorical and numerical columns
+    categorical_cols = X.select_dtypes(include=['object']).columns
+    numerical_cols = X.select_dtypes(include=['float64']).columns
+
+    # Preprocessing for numeric and categorical data
+    numeric_transformer = StandardScaler()
+    categorical_transformer = OneHotEncoder(handle_unknown='ignore')
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numeric_transformer, numerical_cols),
+            ('cat', categorical_transformer, categorical_cols)
+        ]
+    )
+
+    # Create directory if not existing
+    if not os.path.isdir(preprocessor_to):
+        os.makedirs(preprocessor_to)
+
+    pickle.dump(preprocessor, open(os.path.join(preprocessor_to, "obesity_preprocessor.pickle"), "wb"))
 
 if __name__ == '__main__':
     main()
